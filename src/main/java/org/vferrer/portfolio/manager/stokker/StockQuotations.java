036ef4706d84c20ec71582b0552aeac20c715a4a
victor-ferrer <vferrer2005@gmail.com>
2015-11-08 11:35:25 +0100

    Added missing class

---
+++ src/main/java/org/vferrer/portfolio/manager/stokker/StockQuotations.java
@@ -0,0 +1,12 @@
+package org.vferrer.portfolio.manager.stokker;
+
+import java.util.Date;
+
+public class StockQuotations 
+{
+	private String stock;
+	
+	private Double value;
+	
+	private Date timeStamp;
+}
