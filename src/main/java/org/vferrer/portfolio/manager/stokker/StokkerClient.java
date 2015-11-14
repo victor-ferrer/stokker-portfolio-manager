42334dc2d636f7c29e05ceb5eff77102277eb1f6
victor-ferrer <vferrer2005@gmail.com>
2015-11-14 16:01:58 +0100

    Fixed Feign problem with eureka server

--- src/main/java/org/vferrer/portfolio/manager/stokker/StokkerClient.java
+++ src/main/java/org/vferrer/portfolio/manager/stokker/StokkerClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(serviceId = "stokker")
+@FeignClient("stokker")
 public interface StokkerClient 
 {
 	@RequestMapping(method = RequestMethod.GET, value = "/stockQuotationJPAs")
